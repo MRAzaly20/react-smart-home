5bad143c3daf9ef0a592b74704f6bd3035901806
MRAzaly20 <muhammadrazalyy@gmail.com>
2024-04-16 08:41:05 +0700

    modified App

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
 import React from "react";
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./auth/Home";
 import Login from "./auth/Login";
 import SignUp from "./auth/Signup";
-import {
-  AuthProvider
-} from "./auth/auth";
+import yourapp from "./App_";
+import { AuthProvider } from "./auth/auth";
 
 function App() {
-  return (
-    <AuthProvider>
-      <div className="app">
-        <BrowserRouter>
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/login" element={<Login />} />
-            <Route exact path="/signup" element={<SignUp />} />
-          </Routes>
-        </BrowserRouter>
-    </div>
-    </AuthProvider>
-  );
+    return (
+        <AuthProvider>
+            <div className='app'>
+                <BrowserRouter>
+                    <Routes>
+                        <Route exact path='/' element={<Home />} />
+                        <Route exact path='/login' element={<Login />} />
+                        <Route exact path='/signup' element={<SignUp />} />
+                        <Route exact path='/app' element={<yourapp />} />
+                    </Routes>
+                </BrowserRouter>
+            </div>
+        </AuthProvider>
+    );
 }
 
 export default App;
--- src/App_.js
+++ src/App_.js
@@ -1,90 +1,80 @@
-import React, {
-  useEffect,
-  useState
-} from 'react';
-import {
-  useForceUpdate
-} from './useForceUpdate';
+import React, { useEffect, useState } from "react";
+import { useForceUpdate } from "./useForceUpdate";
 
-import Linked from './operator_panel/link-panel.js';
-import Humidity from './operator_panel/humidity.js';
-import WheaterUI from './operator_panel/wheater.js';
-import NavBar from './operator_panel/NavBar.js';
-import "./App.css"
+import Linked from "./operator_panel/link-panel.js";
+import Humidity from "./operator_panel/humidity.js";
+import WheaterUI from "./operator_panel/wheater.js";
+import NavBar from "./operator_panel/NavBar.js";
+import "./App.css";
 
-
-function App() {
-
-  const [data,
-    setData] = useState({
-      message: 'Hello, Flask!'
+function yourapp() {
+    const [data, setData] = useState({
+        message: "Hello, Flask!"
     });
 
-  const forceUpdate = useForceUpdate();
+    const forceUpdate = useForceUpdate();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const result = await sendData(data);
-    console.log(result);
-  };
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const result = await sendData(data);
+        console.log(result);
+    };
 
-  const handleChange = (e) => {
-    setData({
-      ...data, [e.target.name]: e.target.value
-    });
-  };
+    const handleChange = e => {
+        setData({
+            ...data,
+            [e.target.name]: e.target.value
+        });
+    };
 
-  const sendData = (data) => {
-    const response = fetch('/api/send_data', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    });
-    const result = response.json();
-    return result;
-  };
-  useEffect(() => {
-    forceUpdate();
-  }, []);
+    const sendData = data => {
+        const response = fetch("/api/send_data", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+        const result = response.json();
+        return result;
+    };
+    useEffect(() => {
+        forceUpdate();
+    }, []);
 
-  ///////////////////////
-  //     FUNGSI 2     //
-  /////////////////////
+    ///////////////////////
+    //     FUNGSI 2     //
+    /////////////////////
 
-  return(
-    <div>
-  <div class="blur"></div>
-    {/*<form onSubmit={handleSubmit}>
+    return (
+        <div>
+            <div class='blur'></div>
+            {/*<form onSubmit={handleSubmit}>
       <input type="text" name="message" onChange={handleChange} />
       <button type="submit">Send Data</button>
     </form>*/}
-    <div>
-     <Humidity />
-     <div class="ui-wh">
-      <WheaterUI />
-    </div>
-    </div>
-      <div>
-      {/*<button value="data kamu" onChange={handleSubmit} onClick={handleChange}>submit</button>*/}
-      <Linked />
-    <div class="nav">
-      <NavBar />
-      </div>
-    </div>
-    <div class="blur2"></div>
-    <div class="blur3"></div>
-    <div class="blur4"></div>
-      {
-      /*<form onSubmit={handleSubmit2}>
+            <div>
+                <Humidity />
+                <div class='ui-wh'>
+                    <WheaterUI />
+                </div>
+            </div>
+            <div>
+                {/*<button value="data kamu" onChange={handleSubmit} onClick={handleChange}>submit</button>*/}
+                <Linked />
+                <div class='nav'>
+                    <NavBar />
+                </div>
+            </div>
+            <div class='blur2'></div>
+            <div class='blur3'></div>
+            <div class='blur4'></div>
+            {/*<form onSubmit={handleSubmit2}>
       <input type="text" name="message2" onChange={handleChange2} />
       <button type="submit">Send Data2</button>
-      </form>*/
-      }
-    </div>
-
-  );
+      </form>*/}
+        </div>
+    );
 }
 
-export default App;
+export default yourapp;
